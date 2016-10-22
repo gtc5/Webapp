2d37b9238b07ac0c95779599b7c90d62f05c2d2a
Justin <justin@justinlau.name>
2016-10-22 13:28:00 -0400

    Implemented UI for endpoints

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,7 +1,17 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope, $http) {
+angular.module('MainCtrl', ['ui.bootstrap']).controller('MainController', function($scope, $http) {
 
-	var url = ""
-	$http.jsonp(url).success(function(data){
-		$scope.items = data;
-	})
+	var url_volunteers = "http://localhost:33033/admin/volunteers?callback=JSON_CALLBACK";
+	var url_donors = "http://localhost:33033/admin/donors?callback=JSON_CALLBACK";
+	var url_deliveries = "http://localhost:33033/admin/deliveries?callback=JSON_CALLBACK";	
+
+	$http.jsonp(url_volunteers).success(function(data){
+		$scope.volunteers = data;
+		console.log(data);
+	});
+	$http.jsonp(url_donors).success(function(data){
+		$scope.donors = data;
+	});
+	$http.jsonp(url_deliveries).success(function(data){
+		$scope.deliveries = data;
+	});
 });
