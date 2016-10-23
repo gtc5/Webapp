da2a49651a2ef4d1490ee1d4f5e83596d40a66a2
Justin <justin@justinlau.name>
2016-10-23 10:23:04 -0400

    Fixed Get requests

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,12 +1,11 @@
 angular.module('MainCtrl', ['ui.bootstrap']).controller('MainController', function($scope, $http) {
 
-	var url_volunteers = "https://gtc5.clive.io:33033/admin/volunteers?callback=JSON_CALLBACK";
-	var url_donors = "https://gtc5.clive.io:33033/admin/donors?callback=JSON_CALLBACK";
-	var url_deliveries = "https://gtc5.clive.io:33033/admin/deliveries?callback=JSON_CALLBACK";	
+	var url_volunteers = "https://gtc5.clive.io/admin/volunteers?callback=JSON_CALLBACK";
+	var url_donors = "https://gtc5.clive.io/admin/donors?callback=JSON_CALLBACK";
+	var url_deliveries = "https://gtc5.clive.io/admin/deliveries?callback=JSON_CALLBACK";	
 
 	$http.jsonp(url_volunteers).success(function(data){
 		$scope.volunteers = data;
-		console.log(data);
 	});
 	$http.jsonp(url_donors).success(function(data){
 		$scope.donors = data;
